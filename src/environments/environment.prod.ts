639b5a108dc3a75a04360127c7326440f75a1b44
System Administrator <root@Antonys-MacBook-Pro.local>
2021-03-26 10:06:19 -0500

    some

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a54853785d72445f0990343754f161ed-115997bd76bebde4.elb.us-west-2.amazonaws.com/api/v0'
 };
 
 /*
